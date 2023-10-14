1aea25796c889559a136e24d3156493835d8e4a9
Anuj Subhash Pradhan <109716992+Anuj-Pradhan235@users.noreply.github.com>
2023-10-14 03:06:48 +0000

    comparisions commit

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,9 @@
+// avoid this type of conversion
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined==0)
+//comparision and check works differently
+//=== strick check
+console.log("2"===2)
+
