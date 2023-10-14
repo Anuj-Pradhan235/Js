27c707a07fd0acc89bbfd6beebe9c867add8ad37
Anuj Subhash Pradhan <109716992+Anuj-Pradhan235@users.noreply.github.com>
2023-10-14 02:33:17 +0000

    comversion_operations commit

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+// // let score="33"
+// // console.log(typeof score);
+// // let valueInNumber=Number(score);// typecasting a string to number
+// // console.log(valueInNumber);//NaN not a number
+// // score=null;
+// // valueInNumber=Number(score);
+// // console.log(valueInNumber); // 0
+// // score=undefined
+// // valueInNumber=Number(score);
+// // console.log(valueInNumber);// NaN
+// // score=true;
+// // valueInNumber=Number(score)
+// // console.log(valueInNumber);//true:-1  false:-0
+
+// let isLoggedIn=1;
+// let booleanLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+// // 1 ->true
+// // 0 ->false
+// // "" ->false
+// // "anuj"->true
+
+// let someNumber=33
+// let someString=String(someNumber);
+// console.log(someString);
+// console.log(typeof(someString));
