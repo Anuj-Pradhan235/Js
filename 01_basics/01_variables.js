8ddd58148c43d7e828ddf85aca960c7360c3bcce
Anuj Subhash Pradhan <109716992+Anuj-Pradhan235@users.noreply.github.com>
2023-10-14 01:33:13 +0000

    datatypes commited

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1234;
+let email="pradhananuj235@gmail.com"
+// var accountPassword="333333333";
+accountPassword="444";
+console.log(accountPassword);
+email="mmm@gmail.com";
+console.log(email);
+let accountState;
+console.table([accountPassword,email,accountState]);
+/*
+prefer not to use the var keyword due to the issues in the block and functional scope
+*/
